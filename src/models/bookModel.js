9b9cd8f66f3722d538148a7442953c187ad859d0
Priyanka9120 <priyanka912066@gmail.com>
2022-12-06 23:03:05 +0530

    new branch

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -2,63 +2,73 @@ const mongoose = require("mongoose");
 const ObjectId = mongoose.Schema.Types.ObjectId
 
 const bookSchema = new mongoose.Schema({
-    title: { 
-        type: String, 
+
+    bookCover: {
+        type: String,
+        require: true,
+        unique: true,
+        trim: true
+
+    },
+
+    title: {
+        type: String,
         required: true,
-        trim: true, 
+        trim: true,
         lowercase: true,
         unique: true
     },
 
-    excerpt: { 
-        type: String, 
-        required: true, 
-        trim: true 
+    excerpt: {
+        type: String,
+        required: true,
+        trim: true
     },
 
-    userId: { 
-        type: ObjectId, 
-        ref:"User", 
-        required: true, 
-        trim: true 
+    userId: {
+        type: ObjectId,
+        ref: "User",
+        required: true,
+        trim: true
     },
 
-    ISBN: { 
-        type: String, 
-        required: true, 
-        trim: true 
+    ISBN: {
+        type: String,
+        required: true,
+        trim: true
     },
 
-    category: { 
-        type: String, 
-        required: true, 
-        trim: true 
+    category: {
+        type: String,
+        required: true,
+        trim: true
     },
 
-    subcategory: { 
-        type:String, 
-        required: true, 
-        trim: true 
+    subcategory: {
+        type: String,
+        required: true,
+        trim: true
     },
 
     reviews: {
-         type: Number, 
-         default: 0 
-        },
-
-    deletedAt: { 
-        type: Date, 
-        default: null },
-
-    isDeleted: { 
-        type: Boolean, 
-        default: false 
+        type: Number,
+        default: 0
     },
 
-    releasedAt: { 
+    deletedAt: {
+        type: Date,
+        default: null
+    },
+
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
+
+    releasedAt: {
         type: String,
-        required:true
-    } 
+        required: true
+    }
 
 }, { timestamps: true })
 
